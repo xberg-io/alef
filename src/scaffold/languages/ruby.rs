e1cfd3f3d6b2fddb767f70ab259d1f58cee55bf4
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-05-29 15:03:59 +0200

    fix(ruby): filter out directories from gemspec files glob

--- src/scaffold/languages/ruby.rs
+++ src/scaffold/languages/ruby.rs
@@ -191,7 +191,7 @@ Gem::Specification.new do |spec|
   spec.required_ruby_version = ">= 3.2.0"
 {metadata}  spec.metadata["rubygems_mfa_required"] = "true"
 
-  spec.files         = Dir.glob(%w[README* LICENSE* lib/**/* ext/**/* sig/**/* Steepfile]).reject {{ |f| f.include?("/native/target/") || f.include?("/native/tmp/") }}
+  spec.files         = Dir.glob(%w[README* LICENSE* lib/**/* ext/**/* sig/**/* Steepfile]).select {{ |f| File.file?(f) }}.reject {{ |f| f.include?("/native/target/") || f.include?("/native/tmp/") }}
   spec.require_paths = ["lib"]
   spec.extensions    = ["ext/{ext_name}/native/extconf.rb"]
 
